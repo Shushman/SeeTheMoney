7fe2895da77599492b72e09fd25f88c40d404e9e
Shushman Choudhury <shushmanchoudhury@gmail.com>
2014-01-09 23:32:40 +0530

    Update image.cpp

--- jni/image.cpp
+++ jni/image.cpp
@@ -1,268 +1,424 @@
 #include <jni.h>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/core/internal.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/ml/ml.hpp>
+#include <cstdio>
+#include <iostream>
+#include<math.h>
+#include<ml.h>
+#include <cstring>
+#include<ctime>
 #include <android/log.h>
 #include <sys/types.h>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/opencv.hpp"
 
-using namespace std;
+#define WIDTH 480
+#define HEIGHT 320
+#define TRAIN_IMAGE 10
+#define VOCAB_SIZE 10000
+
 using namespace cv;
+using namespace std;
+
+SiftFeatureDetector detector(500);
+BOWImgDescriptorExtractor bowDE(new SiftDescriptorExtractor(),new FlannBasedMatcher());
+Mat vocabulary, inverted_index[VOCAB_SIZE],labels;
+Mat keypoints_imgs;
+string keypath;
+float *dot_pro;int *indices;
+int geo_score[20]={0};
+int correct=0;
+int decision[6]={0};
+int testval = 5;
 
 
-int getResult(Mat&);//Obtains the probable result, given a candidate image - 10 is index 0, 20 is 1, and so on
-void getCannyParams(Mat&,int&,int&);//Obtains Canny edge parameters based on grayscale values
-Mat getSegmented(Mat);//Obtains a segmented version of the image around the note
-Mat c_test;
-
-Mat submats[6];//6 matrices for data of each clusters
-Mat labmats[6];//6 label matrices containing the label of each cluster
-CvKNearest knns[6];//For obtaining the nearest neighbour in each cluster
-TermCriteria criteria(TermCriteria::MAX_ITER + TermCriteria::EPS, 100, 0.1);
-float values[3] = {0};
-
-
-Mat& cluster_test(Mat& test_mat,int clusters=50)
+int max_decision()
 {
-
-	if(test_mat.rows > clusters)
+	//Returns the most likely decision. Each integer maps to a particular currency note, or an unsure result
+	int max_index=-1;
+	int max=0;
+	for(int i=0;i<6;i++)
 	{
-		BOWKMeansTrainer bowtrainer(clusters,criteria,1,KMEANS_PP_CENTERS); //num clusters
-		bowtrainer.add(test_mat);
-		c_test = bowtrainer.cluster();
+			if(max<decision[i])
+			{
+					max_index=i;
+					max=decision[i];
+			}
 	}
-	else
-	{
-		c_test=test_mat.clone();
-	}
-	__android_log_write(ANDROID_LOG_INFO, "JNI", "Query image clustered");
-	return c_test;
+	if(max<60) //Uncertain result
+		return -2;
+	return max_index;
+}
+
+Mat grabcut_seg(Mat input)
+{
+		//Segmentation using two iterations of GrabCut
+	    Mat inp2;
+        resize(input,inp2,Size(640,(int)(input.rows*(640.0/input.cols))));
+        Mat img;
+        resize(input,img,Size(90,(int)(input.rows*(90.0/input.cols))));
+
+        int thresh=(int)(0.9*img.rows*img.cols);
+        int count=0;
+        Rect rect=Rect(5,5,img.cols-10,img.rows-10);
+        Mat  bgdModel, fgdModel;
+        Mat mask;
+
+        //First iteration to obtain initial mask
+        grabCut(img, mask,rect,bgdModel,fgdModel,1,GC_INIT_WITH_RECT );
+
+        for(int i=0;i<mask.rows;i++)
+        {
+                for(int j=0;j<mask.cols;j++)
+                {
+                        if(mask.at<uchar>(i,j)==0||mask.at<uchar>(i,j)==2)
+                        {
+                                mask.at<uchar>(i,j)=2;
+                                count++;
+                        }
+                }
+        }
+
+        if(count>0 && count<thresh) //Within the specified thresholds
+        {
+                grabCut(img, mask,rect,bgdModel,fgdModel,1,GC_INIT_WITH_MASK );
+        }
+
+        else
+        {
+                for(int i=0;i<mask.rows;i++)
+                {
+                        for(int j=0;j<mask.cols;j++)
+                        {
+                                if(i>0.2*mask.rows && i<0.8*mask.rows)
+                                {
+                                        if(j>0.2*mask.cols && j<0.8*mask.cols)
+                                        {
+                                                mask.at<uchar>(i,j)=3;
+                                        }
+
+                                }
+                        }
+                }
+        }
+        cv::compare(mask,cv::GC_PR_FGD,mask,cv::CMP_EQ);
+
+        Mat mask2;
+        resize(mask,mask2,Size(inp2.cols,inp2.rows),0,0,CV_INTER_LINEAR);
+        return mask2;
 }
 
 
-int getResult(Mat& test_image){
+int keypoints_in_mask(Mat mask,int i,int j)
+{
+        for(int k=i-1;k<i+1;k++)
+        {
+            for(int l=j-1;l<j+1;l++)
+            {
+                if(mask.at<uchar>(k,l)<10)
+                {
+                    return 0;
+                }
+            }
+        }
+    return 1;
+}
 
-	double res[6] = {0};//To store the total weighted votes based on distance
-	double mins[6];//To store the votes for each descriptor
-   	Mat descriptors;
-	__android_log_write(ANDROID_LOG_INFO, "JNI", "Beginning feature detection and extraction");
+void remove_keypoints(vector<KeyPoint> &keypoints,Mat mask)
+{
+	//Removes keypoints not in the mask
+    for(int i=0;i<keypoints.size();)
+    {
+    	int x=keypoints[i].pt.x;
+    	int y=keypoints[i].pt.y;
 
-	OrbFeatureDetector detector(500, 1.2f,8,15,0,2,0,15);
-	FREAK extractor(true,true,15);
-
-	Mat nearest=cvCreateMat(1, 1, CV_32FC1);//To store the label of the nearest cluster (trivial parameter)
- 	Mat neighbor_dist=cvCreateMat(1,1, CV_32FC1);//To store the nearest neighbour distance
-  	vector<KeyPoint> keypoints;//For the key-points detected
-
-  	detector.detect(test_image,keypoints);//Detection of key-points
-  	extractor.compute(test_image, keypoints, descriptors);//Extraction of descriptors
-
-  	Mat des1,des2;
-  	int i,j,k,l,c,temp;
-	double dist,min;
-
-	float response;
-  	descriptors.convertTo(des1,CV_32FC1);
-
-  	//The module below votes for the possible results depending on distances from each cluster
-	if(des1.rows>120){
-		c = des1.rows/5;
-		des2 = cluster_test(des1,c);
-  		for(i=0;i<des2.rows;i++){
-			for(j=0;j<6;j++){
-				response = knns[j].find_nearest(des2.row(i),1,0,0,&nearest,&neighbor_dist);
-				mins[j]=(neighbor_dist.at<float>(0,0));
-			}
-			//The minimum distance is obtained and the other distances' ratio to the minimum taken
-			min=mins[0];
-			for(j=1;j<6;j++){
-				if(mins[j]<min)
-					min=mins[j];
-			}
-			for(j=0;j<6;j++){
-				res[j]+=mins[j]/min;
-			}
-		}
-  		int ind[6] = {0,1,2,3,4,5};
-  		//The index with the minimum votes is the likely solution
-		for(i=0;i<5;i++){
-			for(j=i+1;j<6;j++){
-				if(res[i]>res[j]){
-					response=res[i];res[i]=res[j];res[j]=response;
-					temp=ind[i];ind[i]=ind[j];ind[j]=temp;
-				}
-			}
-		}
-		values[0]=ind[0]; values[1]=res[0]/c;
-		if(res[1]-res[0]>c/20.0){
-			return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-	else
-  	{
-		values[0]=values[1]=-1;
-  		return -1; //Fewer than 60 descriptors will require the user to take another image
-  	}
+    	if(!keypoints_in_mask(mask,y,x))
+    	{
+    		keypoints.erase(keypoints.begin()+i);
+    	}
+    	else
+    	{
+    		i++;
+    	}
+   }
 }
 
 
-void getCannyParams(Mat& src, int& low, int& high){
+int arg_sort()
+{
+	//Ranks the retrieved images based on the similarity with the given image
+    indices=(int *) malloc(sizeof(int)*labels.rows);
+    for(int i=0;i<labels.rows;i++)
+    {
+        indices[i]=i;
+    }
 
-	__android_log_write(ANDROID_LOG_INFO, "JNI", "Obtaining parameters for Canny edge detection");
-	int i,j,tot=src.rows*src.cols,pos,sum=0;
-	int bins[256] = {0};
+    int tmp_index; float tmp;
 
-	for(i=0;i<src.rows;i++){
-		for(j=0;j<src.cols;j++){
-			pos = (int)(src.at<uchar>(i,j));
-			bins[pos]++;
-		}
-	}
-	for(i=0;i<256;i++){
-		sum+=bins[i];
-		if(sum>tot/2)//Median pixel value
-			break;
-	}
-	low=(5*i)/12;
-	high = 2*low;
+    for(int i=0;i<labels.rows;i++)
+    {
+        for(int j=i+1;j<labels.rows;j++)
+        {
+            if(dot_pro[i]<dot_pro[j])
+            {
+                tmp=dot_pro[i];
+                dot_pro[i]=dot_pro[j];
+                dot_pro[j]=tmp;
+
+                tmp_index=indices[i];
+                indices[i]=indices[j];
+                indices[j]=tmp_index;
+            }
+        }
+    }
+    for(int i=0;i<10;i++)
+    {
+    	__android_log_print(ANDROID_LOG_INFO, "JNI","\n%d : %d :%f",i,indices[i],dot_pro[i]);
+    }
+   return indices[0];
+
 }
 
 
-Mat getSegmented(Mat src){
+int get_top_geoscore()
+{
+	//Computes the highest score for geometric verification
+    int max=0, max_index=0;
+    for(int i=0;i<20;i++)
+    {
+        if(max<geo_score[i])
+        {
+            max=geo_score[i];
+            max_index=i;
+        }
+    }
+    geo_score[max_index]=-10;
+    __android_log_print(ANDROID_LOG_INFO, "JNI","\nMax : %d ,%d\n",max,indices[max_index]);fp_k    //printf("\n\nresponse : %d\tcorrect:%d\n\n",response,correct);ey
+    decision[labels.at<uchar>(indices[max_index],0)]= decision[labels.at<uchar>(indices[max_index],0)]+max;
+    return indices[max_index];
+}
 
-	__android_log_write(ANDROID_LOG_INFO, "JNI", "Segmenting the image");
-	Mat copy = src.clone();
-	Mat res, dst;
-	int threshold=100,lt=50,ht=230,step=10;
-	GaussianBlur(src,src,Size(3,3),0,0);//Image smoothed to remove small distortions
+void re_rank_geo(vector<KeyPoint> keypoints, vector<vector<int> > pointIdxsOfClusters)
+{
+	//Performs spatial re-ranking based on geometric verification
+    for(int i=0;i<20;i++)
+    {
+        geo_score[i]=0;
+        Mat point1, point2;
+        stringstream ss;
+        //Reads keypoint locations from folder
+        ss<<keypath<<"/"<<indices[i]<<".txt";
+        FILE *fp_key;
+        fp_key=fopen(ss.str().c_str(),"r");
+        int vocab_id=-1,x=-1,y=-1;
 
-	int minx,miny,maxx,maxy;
-	getCannyParams(src,lt,ht);//Canny edge parameters obtained
-	vector<Vec2f> lines;
-	vector<Point2f> intersections;
-	Canny(src,dst,lt,ht,3);
-	int x1,y1,l=10000,i,j,target;
-	__android_log_write(ANDROID_LOG_INFO, "JNI", "Detecting lines with Hough Transform");
+        for(int j=0;j<pointIdxsOfClusters.size();j++)
+        {
+            for(int k=0;k<pointIdxsOfClusters[j].size() && !feof(fp_key);k++)
+            {
+                Mat temp=Mat(1,2,CV_32F);
+                temp.at<float>(0,0)=keypoints[pointIdxsOfClusters[j][k]].pt.x;
+                temp.at<float>(0,1)=keypoints[pointIdxsOfClusters[j][k]].pt.y;
 
-	//Houghlines is run repeatedly till the number of lines is reasonable
-	HoughLines(dst, lines, 1, CV_PI/180, threshold, 0, 0);
-	l = lines.size();
-	target = (l/10>500)?l/10:500;
+                while(!feof(fp_key))
+                {
+                    fscanf("%d%d%d",&vocab_id,&x,&y);
+                    if(vocab_id>j)
+                    {
 
-	while(l>target){
-		threshold+=step;
-		HoughLines(dst, lines, 1, CV_PI/180, threshold, 0, 0 );
-		l = lines.size();
-	}
+                        break;fp_k    //printf("\n\nresponse : %d\tcorrect:%d\n\n",response,correct);ey
+                    }
+                    if(vocab_id==j)
+                    {
+                        Mat temp2=Mat(1,2,CV_32F);
+                        temp2.at<float>(0,0)=x;
+                        temp2.at<float>(0,1)=y;
+                        point1.push_back(temp);
+                        point2.push_back(temp2);
+                        break;
+                    }
+                }
+            }
 
-	//Points of intersection of perpendicular lines are considered
-	for(i = 0; i< l; i++ ){
-		float rho1 = lines[i][0], theta1 = lines[i][1];
-		for(j=0;j<l;j++){
-			if(j==i) continue;//Same lines are not considered
-			float rho2,theta2;
-			rho2 = lines[j][0]; theta2 = lines[j][1];
-			float diff = abs(theta1-theta2);
-			if(abs(diff-1.57)>0.05)//Lines should be almost perpendicular
-				continue;
-			y1 = (int)abs((rho1*cos(theta2)-rho2*cos(theta1))/sin(theta1-theta2));
-			x1 = (int)abs((rho1*sin(theta2)-rho2*sin(theta1))/sin(theta2-theta1));
+        }
+	fclose(fp_key);
 
-			if(x1>0&&x1<src.cols&&y1>0&&y1<src.rows){
-				intersections.push_back(Point2f(x1,y1));
-			}
-		}
-	}
+    if(point1.rows>0)
+    {
 
-	l = intersections.size();
-	if(l==0){
-		//No intersections could be accurately computed, so whole image taken
-		__android_log_write(ANDROID_LOG_INFO, "JNI", "No intersections in image");
-		res = copy(Range(60,src.rows-60),Range(80,src.cols-80));
-		return res;
-	}
+        Mat out;
+        Mat fundamentalMat=findFundamentalMat(point1,point2,FM_RANSAC,3.0,0.99, out );
+        for(int j=0;j<out.rows;j++)
+        {
+            geo_score[i]=geo_score[i]+out.at<uchar>(j,0);
+        }
+    }
+    }
 
-	//The maximum bounding box from intersection points is considered
-	Point2f p = intersections[0];
-	minx=maxx=p.x;miny=maxy=p.y;
-	for(i=1;i<l;i++){
-		Point2f p =intersections[i];
-		if(p.x<minx) minx=p.x;
-		else if(p.x>maxx) maxx=p.x;
-		if(p.y<miny) miny=p.y;
-		else if(p.y>maxy) maxy=p.y;
-	}
-	//In case the bounding box is too small
-	if(maxy-miny<400||maxx-minx<400){
-		res = copy(Range(60,src.rows-60),Range(80,src.cols-80));
-		return res;
-	}
+}
 
-	res = copy(Range(miny,maxy),Range(minx,maxx));
-	return res;
+
+int sort_by_dot_product(Mat descriptors)
+{
+	//Sets up the ranking of similar images by computing the distances based on dot product
+    dot_pro=(float *) malloc(sizeof(float)*labels.rows);
+
+    for(int i=0;i<labels.rows;i++)
+    {
+        dot_pro[i]=0;
+    }
+
+    for(int i=0;i<VOCAB_SIZE;i++)
+    {
+        for(int j=0;j<inverted_index[i].rows;j++)
+        {
+            dot_pro[ (int)inverted_index[i].at<float>(j,0) ] = dot_pro[ (int)inverted_index[i].at<float>(j,0) ] + inverted_index[i].at<float>(j,1)*descriptors.at<float>(0,i);
+        }
+    }
+
+    return(arg_sort());
+
+
+}
+
+int test(Mat img2)
+{
+	//Obtains the result for a particular image.
+    Mat img;
+    cvtColor(img2,img,CV_BGR2GRAY);
+
+    __android_log_write(ANDROID_LOG_INFO, "JNI", "Before grabcut");
+    Mat mask=grabcut_seg(img2);
+    __android_log_write(ANDROID_LOG_INFO, "JNI", "After grabcut");
+
+    if(img.cols>640)
+    {
+        resize(img,img,Size(640,img.rows*(640.0/img.cols)));
+    }
+    __android_log_write(ANDROID_LOG_INFO, "JNI", "Before SIFT");
+    vector<KeyPoint> keypoints;
+    detector.detect(img,keypoints);
+    __android_log_write(ANDROID_LOG_INFO, "JNI", "After SIFT detection");
+
+    remove_keypoints(keypoints,mask);
+    __android_log_print(ANDROID_LOG_INFO, "JNI", "%d keypoints after masking",keypoints.size());
+
+    if(keypoints.size()<20)
+    {
+        return -1;
+    }
+
+    Mat descriptors;
+    vector<vector<int> > pointIdxsOfClusters;
+    bowDE.compute(img,keypoints,descriptors,&pointIdxsOfClusters);
+    __android_log_print(ANDROID_LOG_INFO, "JNI", "After computing descriptors");
+    int dot_result=sort_by_dot_product(descriptors);
+    __android_log_print(ANDROID_LOG_INFO, "JNI", "After computing dot product");
+    re_rank_geo(keypoints,pointIdxsOfClusters);
+    __android_log_print(ANDROID_LOG_INFO, "JNI", "After spatial re-ranking");
+
+    int res=get_top_geoscore();
+    int result=labels.at<uchar>(res,0);
+    int result1=result;
+    for(int i=0;i<10;i++)
+    {
+        res=get_top_geoscore();
+        result=labels.at<uchar>(res,0);
+    }
+
+    __android_log_write(ANDROID_LOG_INFO, "JNI", "After geometric verification");
+
+    int response=max_decision();
+    if(response==-1)
+    {
+       return -2;
+    }
+        for(int i=0;i<6;i++)
+        {
+                decision[i]=0;
+        }
+     __android_log_write(ANDROID_LOG_INFO, "JNI", "After decision");
+    return response;
+
 
 }
 
 extern "C" {
 
-JNIEXPORT float JNICALL Java_com_example_testocv3_MainActivity_getimage(JNIEnv* env, jobject  obj, jstring ymlpath, jstring fname);
+JNIEXPORT void JNICALL Java_com_example_seethemoney_MainActivity_loaddata(JNIEnv* env, jobject  obj, jstring ymlpath, jstring indexpath, jstring labelpath, jstring keyptspath);
 
-JNIEXPORT float JNICALL Java_com_example_testocv3_MainActivity_getimage(JNIEnv* env, jobject obj, jstring ymlpath, jstring fname)
+JNIEXPORT void JNICALL Java_com_example_seethemoney_MainActivity_loaddata(JNIEnv* env, jobject  obj, jstring ymlpath, jstring indexpath, jstring labelpath, jstring keyptspath)
 {
-	//The filename for the image as stored in the phone, is read here
-	const char *str = env->GetStringUTFChars(fname,0);
-	Mat img = imread(str,0);
-	env->ReleaseStringUTFChars(fname,str);
 
 	//The vocabulary file in memory is read here
-	str = env->GetStringUTFChars(ymlpath,0);
-	FileStorage fs(str,FileStorage::READ);
-	env->ReleaseStringUTFChars(ymlpath,str);
+	const char *strvocab = env->GetStringUTFChars(ymlpath,0);
+	FileStorage fs_vocab(strvocab, FileStorage::READ );
+	env->ReleaseStringUTFChars(ymlpath,strvocab);
 	__android_log_write(ANDROID_LOG_INFO, "JNI", "Vocabulary file has been loaded");
 
-	int i,j;
+	const char *strindex = env->GetStringUTFChars(indexpath,0);
+	FileStorage fs_inv(strindex, FileStorage::READ );
+	env->ReleaseStringUTFChars(indexpath,strindex);
+	__android_log_write(ANDROID_LOG_INFO, "JNI", "Index file has been loaded");
+
+	const char *strlabels = env->GetStringUTFChars(labelpath,0);
+	FileStorage fs_labels(strlabels, FileStorage::READ );
+	env->ReleaseStringUTFChars(labelpath,strlabels);
+	__android_log_write(ANDROID_LOG_INFO, "JNI", "Labels file has been loaded");
+
+	const char *strkeypoints = env->GetStringUTFChars(keyptspath,0);
+	keypath=string(strkeypoints);
+	env->ReleaseStringUTFChars(keyptspath,strkeypoints);
+	__android_log_write(ANDROID_LOG_INFO, "JNI", "Keypoints file has been accessed");
+
 	Mat train_mat;
 	Mat label_mat;
-	fs["train_mat"] >> train_mat;//The training data matrix
-	fs["label_mat"] >> label_mat;//The label data matrix
+	fs_vocab["vocabulary"] >> vocabulary;//The training data matrix
+	vocabulary.convertTo(vocabulary,CV_32F);
+	fs_labels["labels"] >> labels;//The label data matrix
 
-	//The training and label matrices for each cluster are populated
-	for(i=0;i<train_mat.rows;i++){
-		j=(int)(label_mat.at<float>(i,0));
-		submats[j].push_back(train_mat.row(i));
-		labmats[j].push_back(label_mat.row(i));
-	}
 
-	//The CvKNearest objects are trained correspondingly
-	for(i=0;i<6;i++){
-		knns[i].train(submats[i],labmats[i]);
-	}
+	for(int i=0;i<VOCAB_SIZE;i++)
+	    {
+	        char n[50];
+	        sprintf(n,"inverted_index_%d",i);
+	        fs_inv[n] >> inverted_index[i];
+	    }
+
+	fs_labels.release();
+	fs_inv.release();
+	fs_vocab.release();
+
+	bowDE.setVocabulary(vocabulary);
+	__android_log_write(ANDROID_LOG_INFO, "JNI", "Loading of yml files done");
+
+	testval = 10;
+}
+}
+
+extern "C" {
+
+JNIEXPORT int JNICALL Java_com_example_seethemoney_MainActivity_getimage(JNIEnv* env, jobject  obj, jstring fname);
+
+JNIEXPORT int JNICALL Java_com_example_seethemoney_MainActivity_getimage(JNIEnv* env, jobject  obj, jstring fname)
+{
+	//__android_log_print(ANDROID_LOG_INFO, "JNI", "Test should be 10 and is %d",testval);
+	//The filename for the image as stored in the phone, is read here
+	const char *str = env->GetStringUTFChars(fname,0);
+	Mat img = imread(str,1);
+	env->ReleaseStringUTFChars(fname,str);
 
 	if(!img.data){
 		__android_log_write(ANDROID_LOG_INFO, "JNI", "Query image did not load properly");
 	}
-	Mat img2,red;
-	resize(img,img2,Size(800,600));
-	Mat img3=getSegmented(img2);
-	double fact = img3.cols/450.0;
-	resize(img3,red,Size(450,(int)(img3.rows/fact)));
-	int val = getResult(red);
-	float res;
-	if(val==-1)
-		return -1.0f;
-	else{
-		res=100*val+10*values[0]+values[1];
-	}
+	int val = test(img);
 	__android_log_write(ANDROID_LOG_INFO, "JNI", "Exiting native code");
-	return res;
+	return val;
 }
 }
-
 
 
 
